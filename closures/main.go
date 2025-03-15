b6d2768107a2dfb6039515df83cccf0a2fa4699f
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-03-15 05:48:42 +0530

    closures example to clarify function behavior and state preservation

--- closures/main.go
+++ closures/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 // example of closures in GO
+// here function adder returns another function tnat takes outer function's variable and modifies it then returns
+// here due to closure, the variable's state is preserved by the inner function
 func adder() func(int) int {
 	sum := 0
 	return func (x int) int {
@@ -16,4 +18,5 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(pos(i), neg(-2 * i))
 	}
+	// in every iteration sum value is preserved and used for the next iteration 
 }
